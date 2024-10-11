df5b3d77c91da6922f4f4b2e662e639bc6ff34f4
ser-jun <svyatokhos@mail.ru>
2024-10-11 22:29:16 +0300

    optimization program.cs

--- Web Server/Program.cs
+++ Web Server/Program.cs
@@ -3,30 +3,24 @@ using Web_Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DataBaseConnection")));
-//builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-if (!app.Environment.IsDevelopment()) { app.UseHttpsRedirection(); }
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.Urls.Add("http://localhost:12300");
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+
 using (var scope = app.Services.CreateScope())
 {
-    var db= scope.ServiceProvider.GetRequiredService<DataContext>();
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
     db.Database.EnsureCreated();
 }
 
-    app.Run();
+app.Run();
